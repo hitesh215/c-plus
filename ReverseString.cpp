b3485e856eae6853c35094d81b2d8c60d2a59fd9
Aahan215 <56781825+Aahan215@users.noreply.github.com>
2021-10-04 13:59:55 +0530

    Update ReverseString.cpp

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-    vector<char> s={'h','e','l','l','o'};
+    vector<char> s={'h','e','l','l','o'};//Hello 
 
     for(int i=0,j=s.size()-1;i<j;i++,j--){
             char temp=s[i];
